e8f1032b0f929025d8fe0cbefc91406b05a98b54
Darshan Patel <darshanpatel47@gmail.com>
2024-08-31 08:30:10 +0530

    Added winston morgan logger

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from './logger.js'
+import morgan from 'morgan'
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 let teaData = []
 let nextId = 1
@@ -51,6 +71,8 @@ app.delete('/teas/:id', (req, res) => {
   if (index === -1) {
     return res.status(404).send('tea not found')
   }
+
+  logger.info(`Deleting teas id ${index}`)
   teaData.splice(index, 1)
   res.status(200).send('deleted')
 })
