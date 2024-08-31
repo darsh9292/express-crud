efe9a2118eeb40d635af1116ba23ce54b223dcec
Darshan Patel <darshanpatel47@gmail.com>
2024-08-31 21:15:33 +0530

    added healthcheck API

--- src/app.js
+++ src/app.js
@@ -11,6 +11,14 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+//routes import
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 const morganFormat = ":method :url :status :response-time ms";
 
